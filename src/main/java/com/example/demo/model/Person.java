5f28df116c2d246eae8d7715c8790fe1443f6c97
Andrew <neebet.2015@yandex.ru>
2023-06-06 00:59:24 +0300

    Start point. But you can see it was not first stage.

---
+++ src/main/java/com/example/demo/model/Person.java
@@ -0,0 +1,13 @@
+package com.example.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.UUID;
+
+public record Person(UUID id, String name) {
+    public Person(@JsonProperty("id") UUID id,
+                  @JsonProperty("name") String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
