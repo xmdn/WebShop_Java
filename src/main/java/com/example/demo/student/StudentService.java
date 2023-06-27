9387ee6b3171e1a6684d9ea7500b65010ed8d97f
Andrew <tnebilyk@gmail.com>
2023-06-27 21:29:48 +0300

    Functional of PUT query.

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,10 +3,12 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -38,5 +40,28 @@ public class StudentService {
                             "student with id " + studentId + " does not exists");
                 }
                 studentRepository.deleteById(studentId);
+    }
+    @Transactional
+    public void updateStudent(long studentId,
+                              String name,
+                              String email) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException(
+                        "student with id " + studentId + " does not exist"));
+                if (name != null && name.length() > 0 &&
+                !Objects.equals(student.getName(), name)) {
+                    student.setName(name);
+                }
+                if (email != null &&
+                    email.length() > 0 &&
+                    !Objects.equals(student.getEmail(), email)) {
+                    Optional<Student> studentOptional = studentRepository
+                            .findStudentByEmail(email);
+                    if (studentOptional.isPresent()) {
+                        throw new IllegalStateException("email taken");
+                    }
+                    student.setEmail(email);
+                }
+
     }
 }
